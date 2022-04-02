ed99a7e0699e397967b151d96bdf8d5381271326
ryanf10 <ryanfernaldy10@gmail.com>
2022-04-02 00:53:06 +0000

    soal3

---
+++ ets/soal3/client_side/.ipynb_checkpoints/tcp_client-checkpoint.py
@@ -0,0 +1,131 @@
+import sys
+import socket
+import json
+import logging
+import xmltodict
+import ssl
+import os
+import random
+import threading
+import time
+import datetime
+
+server_address = ('172.16.16.102', 12000)
+
+def make_socket(destination_address='localhost',port=12000):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (destination_address, port)
+        logging.warning(f"connecting to {server_address}")
+        sock.connect(server_address)
+        return sock
+    except Exception as ee:
+        logging.warning(f"error {str(ee)}")
+
+def make_secure_socket(destination_address='localhost',port=10000):
+    try:
+        #get it from https://curl.se/docs/caextract.html
+
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.verify_mode=ssl.CERT_OPTIONAL
+        context.load_verify_locations(os.getcwd() + '/domain.crt')
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (destination_address, port)
+        logging.warning(f"connecting to {server_address}")
+        sock.connect(server_address)
+        secure_socket = context.wrap_socket(sock,server_hostname=destination_address)
+        logging.warning(secure_socket.getpeercert())
+        return secure_socket
+    except Exception as ee:
+        logging.warning(f"error {str(ee)}")
+
+def deserialisasi(s):
+    logging.warning(f"deserialisasi {s.strip()}")
+    return json.loads(s)
+    
+
+count_error = 0
+def send_command(command_str,is_secure=False):
+    alamat_server = server_address[0]
+    port_server = server_address[1]
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# gunakan fungsi diatas
+    if is_secure == True:
+        sock = make_secure_socket(alamat_server,port_server)
+    else:
+        sock = make_socket(alamat_server,port_server)
+
+    # logging.warning(f"connecting to {server_address}")
+    try:
+        # logging.warning(f"sending message ")
+        sock.sendall(command_str.encode())
+        # Look for the response, waiting until socket is done (no more data)
+        data_received="" #empty string
+        while True:
+            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
+            data = sock.recv(16)
+            if data:
+                #data is not empty, concat with previous content
+                data_received += data.decode()
+                if "\r\n\r\n" in data_received:
+                    break
+            else:
+                # no more data, stop the process by break
+                break
+        # at this point, data_received (string) will contain all data coming from the socket
+        # to be able to use the data_received as a dict, need to load it using json.loads()
+        hasil = deserialisasi(data_received)
+        # logging.warning("data received from server:")
+        return hasil
+    except Exception as ee:
+        # logging.warning(f"error during data receiving {str(ee)}")
+        count_error = count_error+1
+        return False
+
+
+
+def getdatapemain(nomor=[],is_secure=False):
+    for key in nomor:
+        cmd=f"getdatapemain {nomor[key]}\r\n\r\n"
+        hasil = send_command(cmd,is_secure=is_secure)
+        print(hasil)
+
+def lihatversi(is_secure=False):
+    cmd=f"versi \r\n\r\n"
+    hasil = send_command(cmd,is_secure=is_secure)
+    return hasil
+    
+thread_count = 5
+request_count = 20
+
+def mythread(nomor,is_secure=False):
+    print(thread_count)
+    threads = {}
+
+    catat_awal = datetime.datetime.now()
+    for i in range(thread_count):
+        print(f"thread {i} akan request pemain {nomor[i]}")
+        waktu = time.time()
+        #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multithread
+        threads[i] = threading.Thread(target=getdatapemain, args=(nomor[i], False))
+        threads[i].start()
+
+    #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan join
+    for i in range(thread_count):
+        threads[i].join()
+
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+    print(f"Jumlah response diterima {request_count-count_error}, jumlah response hang {count_error}")
+
+if __name__=='__main__':
+    #jumlah request akan dibagi sama rata untuk tiap thread
+    nomor = {}
+    for i in range(thread_count):
+        nomor[i] = {}
+        for j in range(request_count // thread_count):
+            nomor[i][j] = random.randint(1,4)
+    
+    mythread(nomor, is_secure=False)
